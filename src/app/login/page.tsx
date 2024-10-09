9be6ee37607a06cbf3ab59ce89ef6b3eedbd67d2
romnoelp <petracorta.rmnl@gmail.com>
2024-10-09 12:04:55 +0800

    Finished the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,8 @@
-"use client"; // Mark this component as a Client Component
+"use client";
 
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation"; // Use next/navigation instead of next/router
+import { signIn, useSession } from "next-auth/react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Login() {
@@ -10,13 +11,44 @@ export default function Login() {
 
   useEffect(() => {
     if (session) {
-      router.push("/app/page"); // Redirect to main page if logged in
+      router.push("/");
     }
   }, [session, router]);
 
-  if (session) return null; // Don't show the login if already logged in
+  if (session) return null;
 
   return (
-    <h1></h1>
+    <div className="relative flex min-h-screen items-center justify-center">
+      <Image
+        src="/hero.png"
+        alt="Hero"
+        layout="fill"
+        objectFit="cover"
+        className="absolute left-0 top-0 z-0"
+      />
+      <div className="absolute inset-0 z-10 bg-black opacity-10" />{" "}
+      <div className="relative z-20 flex h-[350px] w-[700px] flex-col items-center justify-center rounded-lg bg-white p-12 shadow-lg">
+        <Image
+          src="/login.png"
+          alt="Login illustration"
+          width={300}
+          height={300}
+          className="mb-6"
+        />
+        <button
+          className="flex items-center rounded-lg bg-[#0038AB] px-4 py-2 text-white transition hover:bg-blue-700"
+          onClick={() => signIn("google")}
+        >
+          <Image
+            src="/google-logo.png"
+            alt="Google logo"
+            width={24}
+            height={24}
+            className="mr-2"
+          />
+          Continue in with Google
+        </button>
+      </div>
+    </div>
   );
 }
